4c6607de4d361f24eee22b085b0c51141572202b
Simon <floogles71@gmail.com>
2023-09-20 15:09:32 +0200

    feat/follow planet on click
    followed object indicator
    
    only display planet name in followed indicator

--- main.ts
+++ main.ts
@@ -3,15 +3,11 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { PlanetObjectType } from './types/types';
 
 let paused = false;
+let followedPlanet: THREE.Object3D<THREE.Object3DEventMap> | null = null;
+let planetNameMap = new Map<THREE.Object3D<THREE.Object3DEventMap>, string>();
 
 const pauseIndicator = document.getElementById('pauseIndicator');
-
-window.addEventListener('keydown', (event) => {
-    if (event.key === ' ') { // Vérifiez si la touche appuyée est Espace
-        paused = !paused; // Inversez l'état de rotationPaused
-        pauseIndicator?.classList.toggle('hidden');
-    }
-});
+const followedObjectIndicator = document.getElementById('followedObjectIndicator');
 
 // Initialisation of the scene / camera / renderer
 let scene = new THREE.Scene();
@@ -23,6 +19,7 @@ renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 document.body.appendChild( renderer.domElement );
+
 
 camera.position.z = 20;
 
@@ -63,7 +60,7 @@ const neptuneMaterial = new THREE.MeshStandardMaterial({map: loader.load('/asset
 
 // ----- Lights -----
 
-const sunLight = new THREE.PointLight(0xffffff, 1);
+const sunLight = new THREE.PointLight(0xffffff, 20);
 sunLight.position.set(0, 0, 0);
 sunLight.castShadow = true;
 sunLight.shadow.mapSize.width = 2048;
@@ -74,44 +71,48 @@ solarSystem.add(sunLight);
 const ambiantLight = new THREE.AmbientLight(0xffffff, 0.01);
 scene.add(ambiantLight);
 
-
-
 // ----- Celestial bodies -----
 const planets: PlanetObjectType[] = [];
 
+// SUN
 const sunGroup = new THREE.Group();
 const sun = new THREE.Mesh(ball, sunTextureMaterial);
 const sunEmissive = new THREE.Mesh(ball, sunEmissiveMaterial);
 sunGroup.add(sun, sunEmissive);
 sun.scale.set(0.6, 0.6, 0.6);
 
-// MERCURY
+planetNameMap.set(sun, 'Sun');
 
+// MERCURY
 const mercury = new THREE.Mesh(ball, mercuryMaterial);
 mercury.scale.set(0.2,0.2,0.2);
 mercury.position.x -= sun.scale.x * 4;
 mercury.castShadow = true;
 mercury.receiveShadow = true;
 
+planetNameMap.set(mercury, 'Mercury');
+
 planets.push({planet: mercury, moons: [], distance: mercury.position.x, step: 0.005, rotationStep: 0.01, o: 45})
 
 // VENUS
-
 const venus = new THREE.Mesh(ball, venusMaterial);
 venus.scale.set(0.3,0.3,0.3);
-venus.position.x -= sun.scale.x * 5;
+venus.position.x -= sun.scale.x * 10;
 venus.castShadow = true;
 venus.receiveShadow = true;
+
+planetNameMap.set(venus, 'Venus');
 
 planets.push({planet: venus, moons: [], distance: venus.position.x, step: 0.0025, rotationStep: 0.01, o: 78})
 
 // EARTH
-
 const earth = new THREE.Mesh(ball, earthMaterial);
 earth.scale.set(0.3,0.3,0.3);
-earth.position.x -= sun.scale.x * 6;
+earth.position.x -= sun.scale.x * 16;
 earth.castShadow = true;
 earth.receiveShadow = true;
+
+planetNameMap.set(earth, 'Earth');
 
 const moon = new THREE.Mesh(ball, moonMaterial);
 moon.scale.set(0.3,0.3,0.3);
@@ -119,56 +120,63 @@ moon.position.x -= earth.position.x - earth.scale.x * 2
 moon.castShadow = true;
 moon.receiveShadow = true;
 
+planetNameMap.set(moon, 'Moon');
+
 planets.push({planet: earth, moons: [{planet: moon, distance: moon.position.x, step: 0.001, rotationStep: 0.01, o:0}
 ], distance: earth.position.x, step: 0.001, rotationStep: 0.01, o: 0})
 
 // MARS
-
 const mars = new THREE.Mesh(ball, marsMaterial);
 mars.scale.set(0.2,0.2,0.2);
-mars.position.x -= sun.scale.x * 7;
+mars.position.x -= sun.scale.x * 22;
 mars.castShadow = true;
 mars.receiveShadow = true;
+
+planetNameMap.set(mars, 'Mars');
 
 planets.push({planet: mars, moons: [], distance: mars.position.x, step: 0.005, rotationStep: 0.01, o: 12});
 
 // JUPITER
-
 const jupiter = new THREE.Mesh(ball, jupiterMaterial);
 jupiter.scale.set(0.6,0.6,0.6);
-jupiter.position.x -= sun.scale.x * 12;
+jupiter.position.x -= sun.scale.x * 28;
 jupiter.castShadow = true;
 jupiter.receiveShadow = true;
+
+planetNameMap.set(jupiter, 'Jupiter');
 
 planets.push({planet: jupiter, moons: [], distance: jupiter.position.x, step: 0.004, rotationStep: 0.01, o: 150});
 
 // SATURN
-
 const saturn = new THREE.Mesh(ball, saturnMaterial);
 saturn.scale.set(0.45,0.45,0.45);
-saturn.position.x -= sun.scale.x * 15;
+saturn.position.x -= sun.scale.x * 34;
 saturn.castShadow = true;
 saturn.receiveShadow = true;
+
+planetNameMap.set(saturn, 'Saturn');
 
 planets.push({planet: saturn, moons: [], distance: saturn.position.x, step: 0.003, rotationStep: 0.01, o: 118});
 
 // URANUS
-
 const uranus = new THREE.Mesh(ball, uranusMaterial);
 uranus.scale.set(0.4,0.4,0.4);
-uranus.position.x -= sun.scale.x * 18;
+uranus.position.x -= sun.scale.x * 40;
 uranus.castShadow = true;
 uranus.receiveShadow = true;
+
+planetNameMap.set(uranus, 'Uranus');
 
 planets.push({planet: uranus, moons: [], distance: uranus.position.x, step: 0.002, rotationStep: 0.01, o: 54});
 
 // NEPTUNE
-
 const neptune = new THREE.Mesh(ball, neptuneMaterial);
 neptune.scale.set(0.35,0.35,0.35);
-neptune.position.x -= sun.scale.x * 25;
+neptune.position.x -= sun.scale.x * 46;
 neptune.castShadow = true;
 neptune.receiveShadow = true;
+
+planetNameMap.set(neptune, 'Neptune');
 
 planets.push({planet: neptune, moons: [], distance: uranus.position.x, step: 0.001, rotationStep: 0.01, o: 305});
 
@@ -187,15 +195,73 @@ for(let planetObject of planets) {
 
 // ----- Controls -----
 
-
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true; // Ajoute un amortissement pour des mouvements plus fluides
-controls.dampingFactor = 0.05; // Facteur d'amortissement (ajustez selon vos besoins)
-controls.rotateSpeed = 0.5; // Vitesse de rotation
-controls.zoomSpeed = 1.2; // Vitesse de zoom
-controls.panSpeed = 0.8; // Vitesse de déplacement
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.rotateSpeed = 0.5;
+controls.zoomSpeed = 1.2;
+controls.panSpeed = 0.8;
 
-// ----- Render & animation -----
+// ----- Events Listeners -----
+
+window.addEventListener('keydown', (event) => {
+    if (event.key === ' ') {
+        paused = !paused;
+        pauseIndicator?.classList.toggle('hidden');
+    }
+});
+
+renderer.domElement.addEventListener('click', onObjectClick, false);
+
+// ----- Functions -----
+
+function followObject(object: THREE.Object3D<THREE.Object3DEventMap>) {
+    camera.position.x = object.position.x;
+    camera.position.y = object.position.y;
+    camera.position.z = object.position.z + 2;
+    camera.lookAt(object.position);
+}
+
+function resetCameraPosition() {
+    camera.position.x = 0;
+    camera.position.y = 0;
+    camera.position.z = 20;    
+    camera.lookAt(solarSystem.position);
+} 
+
+
+function onObjectClick(event) {
+    const mouse = new THREE.Vector2();
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+    const raycaster = new THREE.Raycaster();
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersects = raycaster.intersectObjects(solarSystem.children);
+
+    if (intersects.length > 0) {
+        const clickedObject = intersects[0].object;
+        console.log('Objet cliqué :', clickedObject);
+
+        if(followedPlanet !== clickedObject) {
+            console.log("1")
+            followedPlanet = clickedObject;
+            if(followedObjectIndicator) {
+                followedObjectIndicator.classList.remove('hidden');
+                followedObjectIndicator.innerText = `${planetNameMap.get(followedPlanet)}`
+            }
+            followObject(followedPlanet);
+        } else {
+            followedPlanet = null;
+            if(followedObjectIndicator) {
+                followedObjectIndicator.classList.add('hidden');
+                followedObjectIndicator.innerText = '';
+            }
+            resetCameraPosition();
+        }
+    }
+}
 
 function rotatePlanet(object: PlanetObjectType) {
     object.planet.position.x = object.distance * Math.cos(object.o);
@@ -209,7 +275,12 @@ function render() {
 
     controls.update();
 
+    if(followedPlanet) {
+        followObject(followedPlanet);
+    }
+
     if (!paused) {
+
         sun.rotation.y += 0.001;
 
         for(let planetObject of planets) {
